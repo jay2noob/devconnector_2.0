08e7669c696db8a930b45b9e3b08eba1530cf41b
John Fields <jjfields24@outlook.com>
2020-07-05 00:36:59 -0400

    pushing changes

---
+++ client/build/precache-manifest.5162286131889ca0c965c424713baf92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dabda7c2e0000b5652a9f617e61ebb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8125a3f9fca22da7afda",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "c04d96349dc15a967ac1",
+    "url": "/static/js/2.82715dcb.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.82715dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8125a3f9fca22da7afda",
+    "url": "/static/js/main.2b049a10.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
